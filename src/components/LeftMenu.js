a655c20602b325ae46766b355161bb129f79aeee
divy111 <shrivastavadivy.111@gmail.com>
2023-03-01 12:32:06 +0530

    first commit

---
+++ src/components/LeftMenu.js
@@ -0,0 +1,14 @@
+// code for leftmenu
+
+import React from 'react'
+import "../Styles/LeftMenu.css";
+function LeftMenu() {
+  return <div className="leftMenu">
+     <div className=".logoContainer"></div>
+     <div className='searchBox'></div>
+
+    </div>;
+  
+}
+
+export  {LeftMenu};
