450345b39687884a035eebcd86190321c1fa301a
divy111 <shrivastavadivy.111@gmail.com>
2023-03-18 22:30:27 +0530

    lest commit

--- src/components/RightMenu.js
+++ src/components/RightMenu.js
@@ -1,8 +1,33 @@
 import React from "react";
 import "../Styles/RightMenu.css";
-
+import { FaBell, FaCrown,FaRegHeart, FaSun, FaCogs } from "react-icons/fa";
+import Profile from "../img/profile.jpeg";
 function RightMenu() {
-  return <div className="rightMenu"></div>;
+  return( <div className="rightMenu">
+    <div className="goPro">
+      <i>
+        <FaCrown/>
+        <p>
+          Go<span>Pro</span>
+          </p>
+        </i>
+       <i>
+        <FaBell/>
+       </i>
+
+       <i>
+        <FaRegHeart/>
+        </i>
+    </div>
+    <div className="profile">
+      <i><FaSun/></i>
+      <i><FaCogs/></i>
+      <div className="profileMage">
+        <img src={Profile} alt=""/>
+      </div>
+    </div>
+  </div>
+  );
 }
 
 export { RightMenu };
